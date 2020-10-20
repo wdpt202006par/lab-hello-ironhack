d02c9dcb67c41782511bbc8b0e2b665768e2e9ef
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-10-20 21:41:13 +0200

    lab done

--- src/index.js
+++ src/index.js
@@ -4,36 +4,39 @@ import './index.css'
 
 const App = () => {
     return (
-        <div>
-            <div>
-                <img src= '../public/images/ironhack-logo.svg'/>
-                <img src= '../public/images/menu-top.svg'/>
+        <div className='container'>
+            <nav className='navigation'>
+            <img src= '../images/ironhack-logo.svg'/>
+                <img src= '../images/menu-top.svg'/>
+            </nav>
+
+            <div className= 'general'>
                 <h1>Say hello to ReactJS</h1>
                 <p>You will learn a Frontend framework from scatch, to become an Ninka Developer</p>
                 <button>Awesome!</button>
             </div>
 
-            <div>
+            <div className= 'onglets'>
                 <div>
-                    <img src='../public/images/icon1.png'></img>
+                    <img className= 'image'src='../images/icon1.png'></img>
                     <h2>Declarative</h2>
                     <p>React makes it painless to create interactive UIs</p>
                 </div>
 
                 <div>
-                    <img src='../public/images/icon2.png'></img>
+                    <img className= 'image' src='../images/icon2.png'></img>
                     <h2>Components</h2>
                     <p>Build encapsulated components that manage thier state</p>
                 </div>
 
                 <div>
-                    <img src='../public/images/icon3.png'></img>
+                    <img className= 'image' src='../images/icon3.png'></img>
                     <h2>Single-way</h2>
                     <p>A set of immutable values are passed to the components</p>
                 </div>
 
                 <div>
-                    <img src='../public/images/icon4.png'></img>
+                    <img className= 'image' src='../images/icon4.png'></img>
                     <h2>JSX</h2>
                     <p>Staticall-typed, designed to run on modern browers</p>
                 </div>

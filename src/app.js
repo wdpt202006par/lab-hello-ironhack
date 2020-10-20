986159858638d427a4a10bb50fcce0329e9ad635
Marine Tomazi <m.tomazi@hotmail.fr>
2020-10-20 19:22:00 +0200

    Iteration 1 et 2 done

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./app.css" 
+
+const App = () =>{
+    return (
+        //Tout le jsx
+        <div className="App">
+            <p>Coucou</p>
+        </div>
+    )
+}
+
+export default App;

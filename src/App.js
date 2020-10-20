1b2efb3e1ff0a108200944fd1da1bdc5a2788fbc
Claire Lacanal <lacanal.claire@gmail.com>
2020-10-20 20:23:31 +0200

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="background">
+            <nav>
+                <img src="/images/ironhack-logo.svg" alt="logo"/>
+                <img src="/images/menu-top.svg" alt="menu"/>;
+            </nav>
+        </div>
+        
+    );
+  };
+   
+  export default App;
